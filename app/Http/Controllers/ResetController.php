789163cd0f6bdb8ca44d7842407c9e62efbd14dd
Greidy Peña <greidykp@gmail.com>
2022-11-16 15:32:58 -0500

    Add reset endpoint

--- app/Http/Controllers/ResetController.php
+++ app/Http/Controllers/ResetController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class ResetController extends Controller
 {
-    //
+    public function reset()
+    {
+        Artisan::call('migrate:fresh');
+        return('OK');
+    }
 }
